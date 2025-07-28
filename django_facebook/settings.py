83415e4660dd68a4977b9e1cc3c01679ae1d5874
ThorKild_ <pro.antoine.masia@gmail.com>
2025-07-28 14:47:31 +0200

    fix static folder

--- django_facebook/settings.py
+++ django_facebook/settings.py
@@ -112,14 +112,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Media files (uploaded by users)
+# Media files (uploads)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
