4744b703f7cfbdbeb981698015aeeff299627794
maelbadet <maelbadet21@gmail.com>
2025-07-28 16:03:41 +0200

    feat: add models on all app created

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django_facebook import settings
+
+
+class Category(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.TextField(blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+
+class Post(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	title = models.CharField(max_length=255)
+	description = models.TextField(blank=True)
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+	photos = models.ImageField(upload_to='photos/posts/', null=True, blank=True)
+	is_story = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.title} - {self.user.username}"
