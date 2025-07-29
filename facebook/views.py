c7db0dddfa774b84b896b2ba5d5a94246b3c8f18
maelbadet <maelbadet21@gmail.com>
2025-07-29 11:42:47 +0200

    feat: add homepage with all posts on it

--- facebook/views.py
+++ facebook/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
+from posts.models import Post
+
 
 def home(request):
 	modal_body_search = render_to_string("components/modal_search_body.html", request=request)
-
+	posts = Post.objects.filter(is_story=False)
 	return render(request, "facebook/home.html", {
 		"modal_body_search": modal_body_search,
+		"posts": posts,
 	})
