31d2dedcc712982ed1f20c4835af8163bbb98545
ThorKild_ <pro.antoine.masia@gmail.com>
2025-07-29 17:21:28 +0200

    feat: améliorer l'affichage et la gestion des informations utilisateur dans l'interface d'administration

--- utilisateurs/admin.py
+++ utilisateurs/admin.py
@@ -4,15 +4,16 @@ from .models import UtilisateurPersonnaliser
 
 @admin.register(UtilisateurPersonnaliser)
 class UtilisateurPersonnaliserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'first_name', 'last_name', 'telephone', 'date_joined', 'is_active']
-    list_filter = ['is_active', 'is_staff', 'is_superuser', 'date_joined']
-    search_fields = ['username', 'email', 'first_name', 'last_name', 'telephone']
+    list_display = ['username', 'email', 'first_name', 'last_name', 'telephone', 'ville', 'est_privee', 'date_joined', 'is_active']
+    list_filter = ['is_active', 'is_staff', 'is_superuser', 'est_privee', 'date_joined']
+    search_fields = ['username', 'email', 'first_name', 'last_name', 'telephone', 'ville']
     ordering = ['-date_joined']
     readonly_fields = ['date_joined', 'last_login']
     
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Informations personnelles', {'fields': ('first_name', 'last_name', 'email', 'telephone', 'photo_profil')}),
+        ('Informations personnelles', {'fields': ('first_name', 'last_name', 'email', 'telephone', 'ville', 'photo_profil')}),
+        ('Paramètres de confidentialité', {'fields': ('est_privee',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Dates importantes', {'fields': ('last_login', 'date_joined')}),
     )
