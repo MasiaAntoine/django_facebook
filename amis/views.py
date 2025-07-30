757fbe0826a2213483a04afab5187d03e9e03687
ThorKild_ <pro.antoine.masia@gmail.com>
2025-07-30 14:11:38 +0200

    feat: améliorer la recherche d'amis avec gestion des accents et mise à jour des résultats en temps réel

--- amis/views.py
+++ amis/views.py
@@ -10,16 +10,24 @@ class RechercherAmisView(TemplateView):
     template_name = 'amis/rechercher.html'
     
     def get_context_data(self, **kwargs):
+        import unicodedata
+        def strip_accents(s):
+            return ''.join(c for c in unicodedata.normalize('NFD', s or '') if unicodedata.category(c) != 'Mn').lower()
+
         context = super().get_context_data(**kwargs)
         query = self.request.GET.get('q', '')
         utilisateurs = []
         modal_body_search = render_to_string("components/modal_search_body.html", request=self.request)
-        
+
         if query:
-            utilisateurs = UtilisateurPersonnaliser.objects.filter(
-                Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(ville__icontains=query)
-            ).exclude(id=self.request.user.id if self.request.user.is_authenticated else None)
-        
+            utilisateurs_qs = UtilisateurPersonnaliser.objects.all().exclude(id=self.request.user.id if self.request.user.is_authenticated else None)
+            query_norm = strip_accents(query)
+            utilisateurs = [u for u in utilisateurs_qs if (
+                query_norm in strip_accents(u.first_name)
+                or query_norm in strip_accents(u.last_name)
+                or query_norm in strip_accents(u.ville)
+            )]
+
         context.update({
             'utilisateurs': utilisateurs,
             'query': query,
