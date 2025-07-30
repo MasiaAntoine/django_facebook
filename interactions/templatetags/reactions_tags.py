122810b411558788778c0cf639fa13bb5ab80e2e
maelbadet <maelbadet21@gmail.com>
2025-07-30 11:06:11 +0200

    feat: add likes on modal pop up

--- interactions/templatetags/reactions_tags.py
+++ interactions/templatetags/reactions_tags.py
@@ -4,9 +4,10 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def count_reactions(grouped_reactions, post_id, react_type):
-    for item in grouped_reactions:
-        if item['post_id'] == post_id and item['type'] == react_type:
-            return item['total']
-    return 0
+	for item in grouped_reactions:
+		if item['post_id'] == post_id and item['type'] == react_type:
+			return item['total']
+	return 0
